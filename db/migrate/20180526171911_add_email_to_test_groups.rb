32099a4934ae590ec5c6c14361033b2634171562
kscairney <kscairney@gmail.com>
2018-05-26 20:23:56 +0000

    db problem fixed

--- db/migrate/20180526171911_add_email_to_test_groups.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToTestGroups < ActiveRecord::Migration
-  def change
-    add_column :test_groups, :email, :references
-  end
-end
--- db/migrate/20180526171919_add_email_to_test_kos.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailToTestKos < ActiveRecord::Migration
-  def change
-    add_column :test_kos, :email, :references
-  end
-end
